6bc52c21ebcf6728d98ff5ee0f63fd5eaa7cf4af
Mike Spencer <mikespencema@gmail.com>
2017-04-26 16:47:18 -0400

    get and store oauth token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import Cookie from 'js-cookie';
+import queryString from 'query-string';
 
+import Login from './Login';
 import Search from './Search';
+
+const getToken = () => {
+  // #access_token=TOKEN&token_type=bearer&expires_in=72576000&scope=rsvp+ageless
+  const { access_token } = queryString.parse(window.location.hash);
+  if(access_token) {
+    Cookie.set('oath_token', access_token);
+    return access_token;
+  }
+  return Cookie.get('oath_token');
+};
 
 class App extends Component {
   render() {
+    const token = getToken();
     return (
       <section className='container'>
-        <Search />
+        { token ?
+          <Search token={token} /> :
+          <Login /> }
         { /* list of shareable categories as default view */ }
       </section>
     );
